589bb99a027d6210eb3b7d1267dcc2f3b6c6b3d2
Judeotine <75664161+judeotine@users.noreply.github.com>
2024-10-24 21:10:37 +0300

    UPDATE

---
+++ sendEmail.php
@@ -0,0 +1,54 @@
+<?php
+
+// Configuration
+$config = [
+    'admin_email' => 'judextine28@gmail.com',
+    'project_name' => 'Jude\'s portfolio',
+    'form_subject' => 'Jude\'s portfolio',
+];
+
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+if(isset($_POST['name']) && isset($_POST['email'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $body = $_POST['body'];
+
+    require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
+
+    $mail = new PHPMailer();
+
+    //smtp settings
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "judextine28@gmail.com";
+    $mail->Password = 'yourpassword';
+    $mail->Port = 465;
+    $mail->SMTPSecure = "ssl";
+
+    //email settings
+    $mail->isHTML(true);
+    $mail->setFrom($email, $name);
+    $mail->addAddress("youremail@gmail.com");
+    $mail->Subject = ("$email ($subject)");
+    $mail->Body = $body;
+
+    if($mail->send()){
+        $status = "success";
+        $response = "Email is sent!";
+    }
+    else
+    {
+        $status = "failed";
+        $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+    }
+
+    exit(json_encode(array("status" => $status, "response" => $response)));
+}
+
+?>
